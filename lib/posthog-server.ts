3fa7079644b0886cdb779717d9e336cf18326db5
SmokeyLowkey <yuriykondakov8@gmail.com>
2026-02-16 00:51:23 -0600

    modified:   components/posthog-provider.tsx 	modified:   lib/posthog-server.ts

--- lib/posthog-server.ts
+++ lib/posthog-server.ts
@@ -4,7 +4,7 @@ let posthogServerClient: PostHog | null = null
 
 export function getPostHogServerClient(): PostHog {
   if (!posthogServerClient) {
-    posthogServerClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    posthogServerClient = new PostHog(process.env.POSTHOG_KEY!, {
       host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       flushAt: 1,
       flushInterval: 0,
